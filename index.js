1f6072173da65f0c07c398693ae03d25c6b82bba
Darlon Castro da Silva <dadocastrosilva@yahoo.com.br>
2021-12-15 21:02:29 -0300

    Altera segunda aula

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function carregamentoPagina() {
-    alert("Mensagem apresentada no carregamento do sistema");
-}
+// function carregamentoPagina() {
+//     alert("Mensagem apresentada no carregamento do sistema");
+// }
+
+$(() => {
+    //codigo executado no carregamento da pagina
+    //alert("Mensagem apresentada no carregamento do sistema");
+
+    $("#btnSalvar").click(() => {
+        console.log("FUNCAO CLICK FUNCIONOU")
+    })
+})
