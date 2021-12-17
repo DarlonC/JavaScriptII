c807ed4fda1e60b2ec3c7b0e236786d679bb24b1
Darlon Castro da Silva <dadocastrosilva@yahoo.com.br>
2021-12-16 21:05:36 -0300

    AULA 3

--- index.js
+++ index.js
@@ -2,11 +2,51 @@
 //     alert("Mensagem apresentada no carregamento do sistema");
 // }
 
+function populaTabela() {
+    if (localStorage.getItem("produtos")) {
+        let produtos = JSON.parse(localStorage.getItem("produtos"))
+
+        $("#tblProdutos tbody").html("")
+        produtos.forEach((produto) => {
+            $("#tblProdutos tbody").append(`<tr>
+            <td>${produto.nome}</td>
+            <td>${produto.quantidade}</td>
+            <td>${produto.valor}</td>
+            <td style="width: 50px"><button type="button" class="btn btn-info" >E</button></td>
+            <td style="width: 50px"><button type="button" class="btn btn-danger" >D</button></td>
+            </tr>`)
+        })
+    }
+}
+
 $(() => {
     //codigo executado no carregamento da pagina
-    //alert("Mensagem apresentada no carregamento do sistema");
+    populaTabela()
 
     $("#btnSalvar").click(() => {
-        console.log("FUNCAO CLICK FUNCIONOU")
+
+        let produto = {}
+        produto.nome = $("nmProduto").val()
+        produto.quantidade = $("quantidade").val()
+        produto.valor = $("vlrProduto").val()
+
+        let listaProdutos = []
+
+        if (localStorage.getItem("produtos")) {
+            listaProdutos = JSON.parse(localStorage.getItem("produtos"))
+        }
+
+        listaProdutos.push(produto)
+
+        localStorage.setItem("produtos", JSON.stringify(listaProdutos))
+
+        alert("Produto salvo com sucesso")
+
+        $("#nmProduto").val("")
+        $("#Quantidade").val("")
+        $("#vlrProduto").val("")
+
+        populaTabela()
+
     })
 })
